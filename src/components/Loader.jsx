6c4fabb41796cfcc157d9a76e6c225cb3c56ed1c
khalteck <khalidoyeneye@gmail.com>
2023-06-07 20:22:58 +0100

    color

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -4,7 +4,7 @@ const Loader = () => {
   return (
     <div className="w-full h-[100vh] fixed top-0 left-0 bg-white flex justify-center items-center z-[100]">
       <div className="md:w-1/3 w-[80%] text-[1.2rem] md:text-[2rem] font-bold p-[25px] md:py-[20px] rounded-2xl flex flex-col gap-4 justify-center items-center">
-        <RingLoader color="#4b2a05" />
+        <RingLoader color="#5e2eeb" />
       </div>
     </div>
   );
